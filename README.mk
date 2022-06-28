c8e0bc1d133868e71b111059a1251954bb0a44db
ramponis <53002381+ramponis@users.noreply.github.com>
2022-06-28 10:33:30 +0200

    Create README.mk

---
+++ README.mk
@@ -0,0 +1,6 @@
+echo "src/gz base https://raw.githubusercontent.com/ramponis/brcm63xx-tch-3.4.11/master/packages/base
+src/gz luci https://raw.githubusercontent.com/ramponis/brcm63xx-tch-3.4.11/master/packages/luci
+src/gz management https://raw.githubusercontent.com/ramponis/brcm63xx-tch-3.4.11/master/packages/management
+src/gz packages https://raw.githubusercontent.com/ramponis/brcm63xx-tch-3.4.11/master/packages/packages
+src/gz routing https://raw.githubusercontent.com/ramponis/brcm63xx-tch-3.4.11/master/packages/routing
+src/gz telephony https://raw.githubusercontent.com/ramponis/brcm63xx-tch-3.4.11/master/packages/telephony" > /etc/opkg/distfeeds.conf
